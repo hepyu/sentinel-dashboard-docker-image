9b08dd7fea49497c8cb05bb232a7a4351df434da
hepengyuan <hpy253215039@163.com>
2020-01-20 17:26:54 +0800

    update

--- docker-image.sentinel-dashboard-skywalking-1.7.1.sh
+++
@@ -1,1 +0,0 @@
-docker build -f ./Dockerfile-sentinel-dashboard-skywalking-1.7.1 -t hpy253215039/sentinel-dashboard-skywalking-agent:1.7.1 .
---
+++ docker-image.sentinel-dashboard.sh
@@ -0,0 +1,1 @@
+docker build -f ./Dockerfile-sentinel-dashboard-1.7.1 -t hpy253215039/sentinel-dashboard:1.7.1 .
